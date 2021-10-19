c0736976d21a9b543e47dbecc6bdb48716d2197b
Kikel Dmitry <kikeldmitry@gmail.com>
2021-10-19 19:08:07 +0300

    work in progress

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,22 @@
 const mobileMenu = () => {
 	let burger = document.querySelector('#burger-btn'),
 		menu = document.querySelector('#main-menu');
-	burger.addEventListener('click', () => {
-		if (!burger.classList.contains('is-active')) {
-			burger.classList.add('is-active');
-			burger.setAttribute('aria-label', 'Close menu')
-			menu.classList.add('is-active');
-		} else {
-			burger.classList.remove('is-active');
-			burger.setAttribute('aria-label', 'Open menu')
-			menu.classList.remove('is-active');
+	if (burger) {
+		burger.addEventListener('click', () => {
+			if (!burger.classList.contains('is-active')) {
+				burger.classList.add('is-active');
+				burger.setAttribute('aria-label', 'Close menu')
+				menu.classList.add('is-active');
+			} else {
+				burger.classList.remove('is-active');
+				burger.setAttribute('aria-label', 'Open menu')
+				menu.classList.remove('is-active');
 
-		}
-	})
+			}
+		})
+	} else {
+		return
+	}
 }
 
 document.addEventListener('DOMContentLoaded', () => {
