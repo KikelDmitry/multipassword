5db3ce451f6ef7100bd8480a807f69553b24a099
Kikel Dmitry <kikeldmitry@gmail.com>
2021-10-19 13:00:24 +0300

    header completed

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
-console.log('main.js');
+const mobileMenu = () => {
+	let burger = document.querySelector('#burger-btn'),
+		menu = document.querySelector('#main-menu');
+	burger.addEventListener('click', () => {
+		if (!burger.classList.contains('is-active')) {
+			burger.classList.add('is-active');
+			burger.setAttribute('aria-label', 'Close menu')
+			menu.classList.add('is-active');
+		} else {
+			burger.classList.remove('is-active');
+			burger.setAttribute('aria-label', 'Open menu')
+			menu.classList.remove('is-active');
+
+		}
+	})
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	mobileMenu();
+})
